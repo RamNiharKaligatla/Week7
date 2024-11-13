e7364f55bed9a3cb4fda43cf7f295f3550065888
KALIGATLA BHAVYAGUNA RAM NIHAR <108124054@OCTA.EDU>
2024-11-13 16:14:59 +0530

    Create MaximumValue.c

---
+++ MaximumValue.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int l, i;
+	printf("Size of array: ");
+	scanf("%d", &l);
+	printf("Elements of array:\n");
+	int *a = (int *)malloc(l*sizeof(int));
+	for(i=0; i<l; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+	int max = a[0];
+	for(i=0; i<l; i++)
+	{
+		if(max < a[i])
+		{
+			max = a[i];
+		}
+	}
+	printf("Maximum value is: %d", max);
+	return 0;
+}
