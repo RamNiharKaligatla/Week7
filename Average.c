181a9acfa8f81d5677de1875c37eb12d74ad3378
Ram Nihar Kaligatla <bhavyagunaram@gmail.com>
2024-11-20 13:13:46 +0530

    Update Average.c

--- Average.c
+++ Average.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
+
 float arr(int a[], int l)
 {
 	int i, s=0;
 	float avg;
+	
 	for(i=0; i<l; i++)
 	{
 		s += a[i];
 	}
 	avg = s/l;
+	
 	return avg;
 }
+
 int main()
 {
 	int i, l;
 	printf("Size of array: ");
 	scanf("%d", &l);
+	
 	int a[l];
 	printf("Elements of array:\n");
 	for(i=0; i<l; i++)
 	{
 		scanf("%d", &a[i]);
 	}
+	
 	float x = arr(a, l);
 	printf("%f", x);
+	
 	return 0;
 }
