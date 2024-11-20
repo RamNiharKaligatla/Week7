bc3bf522324a2f1cb61c0a0b5805225ff032b217
Ram Nihar Kaligatla <bhavyagunaram@gmail.com>
2024-11-20 13:17:08 +0530

    Update MaximumValue.c

--- MaximumValue.c
+++ MaximumValue.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int main()
 {
 	int l, i;
 	printf("Size of array: ");
 	scanf("%d", &l);
+	
 	printf("Elements of array:\n");
 	int *a = (int *)malloc(l*sizeof(int));
 	for(i=0; i<l; i++)
 	{
 		scanf("%d", &a[i]);
 	}
+	
 	int max = a[0];
 	for(i=0; i<l; i++)
 	{
@@ -20,5 +23,6 @@ int main()
 		}
 	}
 	printf("Maximum value is: %d", max);
+	
 	return 0;
 }
