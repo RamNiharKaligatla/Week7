4e932236bb5531608988a8764b7404509a06ef59
Ram Nihar Kaligatla <bhavyagunaram@gmail.com>
2024-11-26 22:17:05 +0530

    Create ReverseString.c

---
+++ ReverseString.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char a[100];
+    printf("Enter string: ");
+    scanf("%s", a);
+    int l = strlen(a);
+
+    char *p = a;
+    for(int i=0; i<=l; i++)
+    {
+        printf("%c", *(p+l-i));
+    }
+
+    return 0;
+}
